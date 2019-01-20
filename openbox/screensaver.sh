7b016a76e651b962c25a7fbafb38c4255d27c477
kappcy <corey7144@gmail.com>
2019-01-20 00:17:13 -0600

    added a suspend timer with a toggle for it and one for on/off. Both settings are stored in a file in .cache

--- openbox/screensaver.sh
+++ openbox/screensaver.sh
@@ -1,8 +1,42 @@
 #!/bin/sh
+conf=$HOME/.cache/screensaver
+if [ -z $1 ]; then
+	true
+	elif [ $1 == toggle ]; then
+	if [ "$(awk '{if (NR ==2) print $3}' $conf)" == "1" ]; then
+		notify-send "AutoSuspend Disabled"
+		sed -i 's/^\(suspend\s*=\s*\).*$/\10/' $conf
+	else
+		notify-send "AutoSuspend Enabled"
+		sed -i 's/^\(suspend\s*=\s*\).*$/\11/' $conf
+	fi
+	else
+	exit 1
+fi
+if [ "$(awk '{if (NR ==1) print $3}' $conf)" == "1" ]; then
+NL='
+'
+case $(pgrep -u $UID -x screensaver.sh) in
+  *"$NL"*)
+	kill -9 $(pgrep -u $UID -x screensaver.sh | head -n1)
+	if [ -z $1 ];then
+	notify-send "Screensaver Disabled"
+	sed -i 's/^\(state\s*=\s*\).*$/\10/' $conf
+	exit 1
+	fi
+	;;
+esac
+else
+	notify-send "Screensaver Enabled"
+	sed -i 's/^\(state\s*=\s*\).*$/\11/' $conf
+fi
 while true; do
 	until [ "$(xprintidle)" -le "600000" ]; do
 		if xset -q  | grep -i "monitor is on" >/dev/null; then
 			xset dpms force off
+		fi
+		if [ "$(awk '{if (NR ==2) print $3}' $conf)" == "1" ] && [ "$(xprintidle)" -ge '3600000' ]; then
+		systemctl suspend
 		fi
 		sleep 20
 	done
@@ -14,7 +48,7 @@ while true; do
 		pacmd list-sink-inputs | grep -B12 ${array2[i]} | grep RUNNING >/dev/null; do
 			sleep 20
 				if ! pacmd list-sink-inputs | grep -B12 ${array2[i]} | grep RUNNING >/dev/null; then
-					sleep 5
+					xdotool keydown Shift_L keyup Shift_L
 					break
 				elif pgrep -u $UID -x i3lock >/dev/null; then
 					break
