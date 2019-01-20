fb2d5fa51d8127718150607892b8f36e9467037f
kappcy <corey7144@gmail.com>
2019-01-20 00:29:00 -0600

    replaced by main screensaver.sh function

--- openbox/toggle-screensaver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if pgrep -u $UID -x screensaver.sh
-then
-   kill -9 $(pgrep -u $UID -x screensaver.sh) &
-   notify-send "Screensaver Disabled"
-else
-   ~/scripts/screensaver.sh &
-   notify-send "Screensaver Enabled"
-fi
