12c81c3ba2f1ccab0d95c7db3cd27c50b2a912e3
kappcy <corey7144@gmail.com>
2019-01-28 17:37:34 -0600

    minor fixes and trying a couple of new things

--- openbox/screensaver.sh
+++ openbox/screensaver.sh
@@ -1,78 +1,81 @@
 #!/bin/sh
 conf=$HOME/.cache/screensaver
+video=( "youtube\|vimeo\|twitch" "bomi" )
+video2=( "Vivaldi" "bomi" )
 NL='
 '
+blank() {
+	if xset -q  | grep -i "monitor is on"; then
+			xset dpms force off
+	fi
+	if ! grep -o 'suspend.*0' $conf && [ "$(xprintidle)" -ge '3600000' ]; then
+			systemctl suspend
+	fi
+}
 if [ ! -e $conf ];then
 	echo "state = 0" > $conf
 	echo "suspend = 1" >> $conf
 fi
 if [ -z $1 ]; then
-	if grep -o 'state.*1' $conf >/dev/null; then
-		case $(pgrep screensaver.sh) in
-		  *"$NL"*)
+	case $(pgrep screensaver.sh) in
+		*"$NL"*)
 			pkill -o screensaver.sh
-			notify-send "Screensaver Disabled"
-			sed -i 's/^\(state\s*=\s*\).*$/\10/' $conf
-			exit 1
 			;;
-		esac
+	esac
+	if grep -o 'state.*1' $conf; then		
+		notify-send "Screensaver Disabled"
+		sed -i 's/^\(state\s*=\s*\).*$/\10/' $conf
+		exit 1
 	else
 		notify-send "Screensaver Enabled"
 		sed -i 's/^\(state\s*=\s*\).*$/\11/' $conf
 	fi
-elif [ $1 == toggle ]; then
-	if grep -o 'suspend.*1' $conf >/dev/null; then
-		notify-send "AutoSuspend Disabled"
-		sed -i 's/^\(suspend\s*=\s*\).*$/\10/' $conf
-	else
-		notify-send "AutoSuspend Enabled"
-		sed -i 's/^\(suspend\s*=\s*\).*$/\11/' $conf
-	fi
-	exit 1
-elif [ $1 == status ]; then
-	notify-send "$(cat $conf)"
-	exit 1
 else
-	exit 1
+	case "$1" in
+		-t | --toggle)
+			if grep -o 'suspend.*1' $conf; then
+				notify-send "AutoSuspend Disabled"
+				sed -i 's/^\(suspend\s*=\s*\).*$/\10/' $conf
+			else
+				notify-send "AutoSuspend Enabled"
+				sed -i 's/^\(suspend\s*=\s*\).*$/\11/' $conf
+			fi
+			exit 1
+			;;
+		-s | --status)
+			notify-send "$(cat $conf)"
+			exit 1
+			;;
+		*)
+			echo "invalid argument: $1"
+			exit 1
+			;;
+	esac
 fi
 while true; do
 	until [ "$(xprintidle)" -le "600000" ]; do
-		if xset -q  | grep -i "monitor is on" >/dev/null; then
-			xset dpms force off
-		fi
-		if ! grep -o 'suspend.*0' $conf && [ "$(xprintidle)" -ge '3600000' ]; then
-			systemctl suspend
-		fi
-		sleep 20
+		blank
+		sleep 5
 	done
 	sleep 5
-	array=( "youtube\|vimeo\|twitch" "bomi" )
-	array2=( "Vivaldi" "bomi" )
-	for i in ${!array[*]}; do
-		while wmctrl -l | grep -i "${array[i]}" >/dev/null && \
-		pacmd list-sink-inputs | grep -B12 ${array2[i]} | grep RUNNING >/dev/null; do
-			sleep 20
-				if ! pacmd list-sink-inputs | grep -B12 ${array2[i]} | grep RUNNING >/dev/null; then
+	for i in ${!video[*]}; do
+		while wmctrl -l | grep -i "${video[i]}" && \
+		pacmd list-sink-inputs | grep -B12 ${video2[i]} | grep RUNNING; do
+			sleep 5
+			echo moo
+				if ! pacmd list-sink-inputs | grep -B12 ${video2[i]} | grep RUNNING; then
 					xdotool keydown Shift_L keyup Shift_L
 					break
-				elif pgrep -u $UID -x i3lock >/dev/null; then
+				elif pgrep -u $UID -x i3lock; then
 					break
 				fi
 		done
 	done
-	while pgrep -u $UID -x i3lock >/dev/null; do
+	while pgrep -u $UID -x i3lock; do
 		until [ "$(xprintidle)" -le "30000" ]; do
-			if xset -q  | grep -i "monitor is on" >/dev/null; then
-				xset dpms force off
-			fi
-			if ! grep -o 'suspend.*0' $conf && [ "$(xprintidle)" -ge '3600000' ]; then
-				systemctl suspend
-			fi
+			blank
 			sleep 2
 		done
 		sleep 2
-		if ! pgrep -u $UID -x i3lock >/dev/null; then
-			break
-		fi
 	done
 done
