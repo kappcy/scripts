17e893605b2016eb3dae2133c4b55f11592a243b
kappcy <corey7144@gmail.com>
2019-02-17 22:15:43 -0600

    just lil changes

--- openbox/screensaver.sh
+++ openbox/screensaver.sh
@@ -5,43 +5,45 @@ video2=( "Vivaldi" "bomi" )
 NL='
 '
 screenoff() {
+	sleep 2
 	if xset -q  | grep -i "monitor is on"; then
 			xset dpms force off
 	fi
-	if ! grep -o 'suspend.*0' $conf && [ "$(xprintidle)" -ge '3600000' ]; then
+	if ! grep -o 'AutoSuspend.*off' $conf && [ "$(xprintidle)" -ge '3600000' ]; then
 			systemctl suspend
+			xdotool keydown Shift_L keyup Shift_L
 	fi
 }
 if [ ! -e $conf ];then
-	echo "state = 0" > $conf
-	echo "suspend = 1" >> $conf
+	echo "Screensaver is off" > $conf
+	echo "AutoSuspend is on" >> $conf
 fi
 if [ -z $1 ]; then
 	case $(pgrep screensaver.sh) in
 		*"$NL"*)
 			pkill -o screensaver.sh
-			if grep -o 'state.*1' $conf; then		
+			if grep -o 'Screensaver.*on' $conf; then		
 				notify-send "Screensaver Disabled"
-				sed -i 's/^\(state\s*=\s*\).*$/\10/' $conf
+				sed -i "1s/on/off/" $conf
 			fi
 			exit 1
 			;;
 		*)
-			if grep -o 'state.*0' $conf; then
+			if grep -o 'Screensaver.*off' $conf; then
 				notify-send "Screensaver Enabled"
-				sed -i 's/^\(state\s*=\s*\).*$/\11/' $conf
+				sed -i "1s/off/on/" $conf
 			fi
 			;;
 	esac
 else
 	case "$1" in
 		-t | --toggle)
-			if grep -o 'suspend.*1' $conf; then
+			if grep -o 'AutoSuspend.*on' $conf; then
 				notify-send "AutoSuspend Disabled"
-				sed -i 's/^\(suspend\s*=\s*\).*$/\10/' $conf
+				sed -i "2s/on/off/" $conf
 			else
 				notify-send "AutoSuspend Enabled"
-				sed -i 's/^\(suspend\s*=\s*\).*$/\11/' $conf
+				sed -i "2s/off/on/" $conf
 			fi
 			exit 1
 			;;
@@ -61,28 +63,21 @@ else
 	esac
 fi
 while true; do
-	until [ "$(xprintidle)" -le "600000" ]; do
-		screenoff
-		sleep 5
-	done
+	until [ "$(xprintidle)" -le "600000" ]; do screenoff; done
 	sleep 5
 	for i in ${!video[*]}; do
 		while wmctrl -l | grep -i "${video[i]}" && \
 		pacmd list-sink-inputs | grep -B12 ${video2[i]} | grep RUNNING; do
 			sleep 5
-				if ! pacmd list-sink-inputs | grep -B12 ${video2[i]} | grep RUNNING; then
-					xdotool keydown Shift_L keyup Shift_L
-					break
-				elif pgrep -u $UID -x i3lock; then
-					break
-				fi
+			if ! pacmd list-sink-inputs | grep -B12 ${video2[i]} | grep RUNNING; then
+				xdotool keydown Shift_L keyup Shift_L
+			elif pgrep i3lock; then
+				break
+			fi
 		done
 	done
-	while pgrep -u $UID -x i3lock; do
-		until [ "$(xprintidle)" -le "30000" ]; do
-			screenoff
-			sleep 2
-		done
+	while pgrep i3lock; do
+		until [ "$(xprintidle)" -le "30000" ]; do screenoff; done
 		sleep 2
 	done
 done
