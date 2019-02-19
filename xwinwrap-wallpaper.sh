7519d2fe012eb904a64bf41f4fcef547dd4aa2db
kappcy <corey7144@gmail.com>
2019-02-18 20:20:53 -0600

    changed the lock to no longer kill xwinwrap but to just pause mpv for both monitors and resume after

--- xwinwrap-wallpaper.sh
+++ xwinwrap-wallpaper.sh
@@ -4,10 +4,18 @@ RMonitor="$HOME/gif-wallpapers/pixels1.gif"
 
 opts="--no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-stop-screensaver"
 
+pause() {
+	wid=($(xdotool search --class mpv))
+	for i in ${!wid[*]}; do
+	xdotool key --window "${wid[i]}" p
+	done
+}
+
+xwinwrap -g 1920x1080+1920+0 -ni -ov -- mpv -wid WID $RMonitor $opts &
+xwinwrap -g 1922x1080-2+0 -ni -ov -- mpv -wid WID $LMonitor $opts &
 while true; do
-	xwinwrap -g 1920x1080+1920+0 -ni -ov -- mpv -wid WID $RMonitor $opts &
-	xwinwrap -g 1922x1080-2+0 -ni -ov -- mpv -wid WID $LMonitor $opts &
 	until pgrep i3lock; do sleep 5; done
-	killall xwinwrap
-	while pgrep i3lock; do sleep 1; done
+	pause
+	while pgrep i3lock; do sleep 2; done
+	pause
 done
