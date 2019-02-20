5fac373c1e41e2ed34f534da2f04cce0d05a78c8
kappcy <corey7144@gmail.com>
2019-02-19 19:12:10 -0600

    pausing didn't work as planned. reverted changes and cleaned up script a bit

--- xwinwrap-wallpaper.sh
+++ xwinwrap-wallpaper.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
-LMonitor="$HOME/gif-wallpapers/pixels3.gif"
-RMonitor="$HOME/gif-wallpapers/pixels1.gif"
-
-opts="--no-osc --no-osd-bar --loop-file --player-operation-mode=cplayer --no-audio --panscan=1.0 --no-stop-screensaver"
-
-pause() {
-	wid=($(xdotool search --class mpv))
-	for i in ${!wid[*]}; do
-	xdotool key --window "${wid[i]}" p
-	done
-}
-
-xwinwrap -g 1920x1080+1920+0 -ni -ov -- mpv -wid WID $RMonitor $opts &
-xwinwrap -g 1922x1080-2+0 -ni -ov -- mpv -wid WID $LMonitor $opts &
+Screen=( "1922x1080-2+0" "1920x1080+1920+0" )
+Screen1=( "$HOME/gif-wallpapers/pixels3.gif" "$HOME/gif-wallpapers/pixels1.gif" )
 while true; do
-	until pgrep i3lock; do sleep 5; done
-	pause
-	while pgrep i3lock; do sleep 2; done
-	pause
+for i in ${!Screen[*]}; do
+	xwinwrap -g ${Screen[i]} -ni -ov -- mpv -wid WID ${Screen1[i]} --no-osc --no-osd-bar --loop-file \
+	--player-operation-mode=cplayer --no-audio --panscan=1.0 --no-stop-screensaver &
+done
+until pgrep i3lock; do sleep 5; done
+killall xwinwrap
+while pgrep i3lock; do sleep 1; done
 done
