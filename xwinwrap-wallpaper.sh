995044be470fd383323be756a7fbccc98768bad7
kappcy <corey7144@gmail.com>
2019-08-06 18:59:31 -0500

    killing xwinwrap while i3lock is running is now handled by another script

--- xwinwrap-wallpaper.sh
+++ xwinwrap-wallpaper.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 Screen=( "1922x1080-2+0" "1920x1080+1920+0" )
 Screen1=( "$HOME/gif-wallpapers/pixels3.gif" "$HOME/gif-wallpapers/pixels1.gif" )
-while true; do
 for i in ${!Screen[*]}; do
 	xwinwrap -g ${Screen[i]} -ni -ov -- mpv -wid WID ${Screen1[i]} --no-osc --no-osd-bar --loop-file \
 	--player-operation-mode=cplayer --no-audio --panscan=1.0 --no-stop-screensaver &
-done
-until pgrep i3lock; do sleep 2; done
-killall xwinwrap
-while pgrep i3lock; do sleep 1; done
 done
